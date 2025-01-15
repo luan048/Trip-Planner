d9e24658c046911829acfb95ffc96111da57fdd2
luan048 <luanmonteiropedrosa@gmail.com>
2025-01-14 21:53:54 -0300

    Created Models

---
+++ models/planerModel.js
@@ -0,0 +1,38 @@
+import {database} from '../database/db.js'
+import { DataTypes, Sequelize } from 'sequelize'
+
+export const PlannerModel = database.define("planner", {
+    id: {
+        type: DataTypes.UUID,
+        unique: true,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+    },
+
+    clientName: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+
+    country: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+
+    travelDate: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+
+    returnDate: {
+        type: DataTypes.STRING(20),
+        allowNull: true
+    },
+
+    tourAttractions: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
